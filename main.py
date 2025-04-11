e1f369939d70717a7e2587c2c994d76043938a1e
vencent lum <vincentlum1997@gmail.com>
2025-04-11 17:31:31 +0800

    update

--- main.py
+++ main.py
@@ -2,17 +2,21 @@ import modal
 
 image = (
     modal.Image.debian_slim()
-
-    .apt_install("git", "libgl1")  # OpenCV 需要这个库
+    .apt_install(
+        "git",
+        "libgl1",  # OpenCV 图形相关
+        "libglib2.0-0",  # OpenCV 多线程相关（libgthread 所属包）
+    )
     .run_commands(
         "git clone https://github.com/lx1764290007/sam2-flask.git /root/sam2"
     )
     .pip_install("gunicorn")
     .pip_install_from_requirements("requirements.txt")
+    .run_commands("echo force rebuild")
     .add_local_file("checkpoints/sam2.1_hiera_base_plus.pt", "/root/sam2/checkpoints/sam2.1_hiera_base_plus.pt",
                     copy=True)  # 模型
 )
-app = modal.App(name="sam2-flask-app", image=image)
+app = modal.App(name="sam2-server", image=image)
 
 
 @app.function(
